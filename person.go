2211e1ae6b16ebf19538f5855dce562b7c81c503
Axel Etcheverry <axel@etcheverry.biz>
2021-02-10 01:44:44 +0100

    feat: add person generator

---
+++ person.go
@@ -0,0 +1,108 @@
+// Copyright 2021 Axel Etcheverry. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package faker
+
+import "math/rand"
+
+// PersonFlags control the behavior of the generator
+type PersonFlags uint16
+
+// PersonFlags enums
+const (
+	PersonFirstName PersonFlags = 1 << iota
+	PersonLastName
+	PersonMan
+	PersonWoman
+
+	PersonAll = PersonFirstName | PersonLastName | PersonMan | PersonWoman
+)
+
+// PersonInfo struct
+type PersonInfo struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Gender    string `json:"gender"`
+}
+
+func (p PersonInfo) String() string {
+	if p.FirstName != "" && p.LastName != "" {
+		return p.FirstName + " " + p.LastName
+	}
+
+	if p.FirstName != "" {
+		return p.FirstName
+	}
+
+	return p.LastName
+}
+
+// PersonGenerator struct
+type PersonGenerator struct {
+	Flags PersonFlags
+}
+
+// NewPersonGenerator return PersonGenerator
+func NewPersonGenerator() *PersonGenerator {
+	return &PersonGenerator{
+		Flags: PersonAll,
+	}
+}
+
+// Generate person
+func (g *PersonGenerator) Generate() *PersonInfo {
+	person := &PersonInfo{}
+
+	if g.Flags&PersonLastName != 0 {
+		person.LastName = getLastName()
+	}
+
+	genders := []string{}
+	firstNames := []string{}
+
+	if g.Flags&PersonFirstName != 0 && g.Flags&PersonMan != 0 {
+		firstNames = append(firstNames, getManFirstName())
+		genders = append(genders, "Male")
+	}
+
+	if g.Flags&PersonFirstName != 0 && g.Flags&PersonWoman != 0 {
+		firstNames = append(firstNames, getWomanFirstName())
+		genders = append(genders, "Female")
+	}
+
+	if n := len(firstNames); n > 1 {
+		i := rand.Intn(n - 1)
+
+		person.FirstName = firstNames[i]
+		person.Gender = genders[i]
+	} else if n == 1 {
+		person.FirstName = firstNames[0]
+		person.Gender = genders[0]
+	}
+
+	return person
+}
+
+func getManFirstName() string {
+	i := rand.Intn(len(dataManFirstNames) - 1)
+
+	return dataManFirstNames[i]
+}
+
+func getWomanFirstName() string {
+	i := rand.Intn(len(dataWomanFirstNames) - 1)
+
+	return dataWomanFirstNames[i]
+}
+
+func getLastName() string {
+	i := rand.Intn(len(dataLastNames) - 1)
+
+	return dataLastNames[i]
+}
+
+// Person return an generated person
+func Person() *PersonInfo {
+	return NewPersonGenerator().Generate()
+}
---
+++ person_test.go
@@ -0,0 +1,82 @@
+// Copyright 2021 Axel Etcheverry. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package faker
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPersonGenerator(t *testing.T) {
+
+	{
+		g := NewPersonGenerator()
+		g.Flags = PersonFirstName
+
+		person := g.Generate()
+
+		assert.Empty(t, person.FirstName)
+		assert.Empty(t, person.LastName)
+		assert.Empty(t, person.Gender)
+	}
+
+	{
+		g := NewPersonGenerator()
+		g.Flags = PersonFirstName | PersonMan
+
+		person := g.Generate()
+
+		assert.NotEmpty(t, person.FirstName)
+		assert.Empty(t, person.LastName)
+		assert.Equal(t, "Male", person.Gender)
+	}
+
+	{
+		g := NewPersonGenerator()
+		g.Flags = PersonFirstName | PersonWoman
+
+		person := g.Generate()
+
+		assert.NotEmpty(t, person.FirstName)
+		assert.Empty(t, person.LastName)
+		assert.Equal(t, "Female", person.Gender)
+
+		assert.Equal(t, person.FirstName, person.String())
+	}
+
+	{
+		g := NewPersonGenerator()
+		g.Flags = PersonLastName
+
+		person := g.Generate()
+
+		assert.Empty(t, person.FirstName)
+		assert.NotEmpty(t, person.LastName)
+		assert.Empty(t, person.Gender)
+
+		assert.Equal(t, person.LastName, person.String())
+	}
+
+	{
+		g := NewPersonGenerator()
+		g.Flags = PersonLastName | PersonFirstName | PersonMan
+
+		person := g.Generate()
+
+		assert.NotEmpty(t, person.FirstName)
+		assert.NotEmpty(t, person.LastName)
+		assert.Equal(t, "Male", person.Gender)
+
+		assert.Equal(t, person.FirstName+" "+person.LastName, person.String())
+	}
+}
+
+func TestPerson(t *testing.T) {
+	person := Person()
+
+	assert.True(t, regexp.MustCompile("(Male|Female)").MatchString(person.Gender))
+}
